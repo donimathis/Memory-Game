b70c8b60185d1abbf02753627239e447a76c6d64
Doni <dmathis@crimson.ua.edu>
2018-07-14 02:33:21 -0400

    New Features
    Added modal buttons, reset moves and added stars

--- js/app.js
+++ js/app.js
@@ -1,7 +1,104 @@
+//Global scopes
 const deck = document.querySelector('.deck');
 let moves = 0; //the moves counter
+let time = 0; //set the clock variable
+let matched = 0;
+let clockOff = true;//whether the clock is on or off.
+let toggledCards = [];
+let clockId;
+
+time = 121;
+displayTime();
+moves = 16
+checkScore();
+
+writeModalStats();
+toggleModal();
+
+function writeModalStats() {
+    const timeStat = document.querySelector('.modal__time');
+    const clockTime = document.querySelector('.clock').innerHTML;
+    const movesStat = document.querySelector('.modal__moves');
+    const starsStat = document.querySelector('.modal__stars');
+    const stars = getStars();
+
+    timeStat.innerHTML = `Time = ${clockTime}`;
+    movesStat.innerHTML = `Moves = ${moves}`;
+    starsStat.innerHTML = `Stars = ${stars}`;
+}
+
+function getStars() {
+    stars = document.querySelectorAll('.stars li');
+    starCount = 0;
+    for (star of stars) {
+        if (star.style.display !== 'none') {
+            starCount++;
+        }
+    }
+    return starCount;
+}
+
+document.querySelector('.modal__cancel').addEventListener('click', () => {
+    toggleModal();
+});
+
+document.querySelector('.modal__replay').addEventListener('click', replayGame);
+
+document.querySelector('.restart').addEventListener('click', resetGame);
+
+document.querySelector('.modal__replay').addEventListener('click', resetGame);
+
+const TOTAL_PAIRS = 8;
+if (matched === TOTAL_PAIRS) {
+  gameOver();
+}
 
 
+function gameOver() {
+  stopClock();
+  writeModalStats();
+  toggleModal();
+}
+
+
+function replayGame() {
+  resetGame();
+  toggleModal();
+}
+
+function resetMoves() {
+  moves = 0;
+  document.querySelector('.moves').innerHTML = moves;
+}
+
+function resetStars() {
+  stars = 0;
+  const starList = document.querySelectorAll('.stars li');
+  for (star of starList) {
+    star.style.display = 'inline';
+  }
+}
+
+
+function resetGame()  {
+  resetClockandTime();
+  resetMoves();
+  shuffleDeck();
+}
+
+function resetClockandTime() {
+  stopClock();
+  clockOff = true;
+  time = 0;
+  displayTime();
+}
+
+function resetCards() {
+  const cards = document. querySelectorAll('.deck li');
+  for(let card of cards) {
+    card.className = 'card';
+  }
+}
 
 // * Create a list that holds all of your cards
 //  array is a list
@@ -88,7 +185,12 @@ function shuffle(array) {
 
 deck.addEventListener('click',event => {
   const clickTarget = event.target;
-
+if (isClickValid(clickTarget)){
+  if(clockOff) {
+    startClock();
+    clockOff = false;
+  }
+}
 
   console.log(toggledCards);
          toggleCard(clickTarget);
@@ -96,7 +198,7 @@ deck.addEventListener('click',event => {
          if (toggledCards.length === 2) {
              checkForMatch(clickTarget);
              addMove(); //moves to increment after cards are toggled
-             checkScore(); //check stars, call function everytime i complete move. 
+             checkScore(); //check stars, call function everytime i complete move.
          }
      }
  });
@@ -139,17 +241,15 @@ function addToggleCard(clickTarget {
 }
 
 
-function checkForMatch ()
-
+function checkForMatch () {
 if (
-  toggleCards[0].firstElementChild.className
-  ===
+  toggleCards[0].firstElementChild.className ===
   toggleCards[1].firstElementChild.className
 ) {
   toggledCards[0].classList.toggle('match');
   toggledCards[1].classList.toggle('match');
   toggledCards = [];
-
+  matched++;
 } else {
   setTimeout (() => {
     toggleCard(toggledCards[0]);
@@ -162,6 +262,40 @@ if (
 
 
 
+function startClock() {
+  time = 0;
+  clockId= setInterval(() => {
+    time++;
+    displayTime();
+    console.log(time);
+  }, 1000);
+}
+startClock();
+
+function displayTime() {
+  const clock = document.querySelector('.clock');
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  if (seconds < 10 ) {
+    clock.innerHTML = `${minutes}:0${seconds}`;
+  } else {
+    clock.innerHTML = `${minutes}:${seconds}`;
+  };
+}
+
+function stopClock() {
+  clearInterval(clockId);
+}
+
+stopClock();
+
+function toggleModal() {
+  const modal = document.querySelector('.modal__backgroud');
+  modal.classList.toggle('hide');
+}
+toggleModal(); //open modal
+toggleModal();//off
 
 // use a loop to attached an event listener to each card
 // it's easier to do that than individually
@@ -171,7 +305,6 @@ if (
 //step1: create a variable which holds and empty earry
 //step2: push click targets into it.
 
-let toggledCards = [];
 
 
 
